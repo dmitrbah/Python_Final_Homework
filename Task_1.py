b4e0bf1d82647b9678a8eb2251cfc20ef34e8af5
Dmitry Bakhmutov <dmitrbah@mail.ru>
2024-10-24 11:50:27 +0300

    Task 1 - Completed

--- Task_1.py
+++ Task_1.py
@@ -1,3 +1,25 @@
 # Напишите скрипт, который логирует разные типы сообщений в разные файлы.
 # Логи уровня DEBUG и INFO должны сохраняться в debug_info.log, а логи уровня WARNING и выше — в warnings_errors.log
 
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+debug_info_handler = logging.FileHandler('debug_info.txt', encoding='utf-8')
+debug_info_handler.setLevel(logging.DEBUG)
+debug_info_handler.setFormatter(formatter)
+logger.addHandler(debug_info_handler)
+
+warnings_errors_handler = logging.FileHandler('warnings_errors.txt', encoding='utf-8')
+warnings_errors_handler.setLevel(logging.WARNING)
+warnings_errors_handler.setFormatter(formatter)
+logger.addHandler(warnings_errors_handler)
+
+logger.debug('Уровень DEBUG')
+logger.info('Уровень INFO')
+logger.warning('Уровень WARNING')
+logger.error('Уровень ERROR')
+logger.critical('Уровень CRITICAL')
